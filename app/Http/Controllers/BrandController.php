8f0d728a867b7ba68bd84ad4ac4ab0051581f278
Mariam Elfeky <mariamelfeky123@gmail.com>
2020-06-04 00:48:18 +0200

    brands initial commit

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Brand;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $brands = Brand::all();
+        return view('brand/index', ['brands'=>$brands])->render();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('brand/create')->render();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->save();
+        // return view('index', $brand)->render();
+        return redirect()->action('BrandController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $brand = Brand::find($id);
+        return view('brand/edit', ['brand' =>$brand])->render();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $brand = Brand::find($id);
+        $brand->name = $request->name;
+        $brand->save();
+        return redirect()->action('BrandController@index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $brand = Brand::find($id);
+        $brand->delete();
+        return redirect()->action(
+            'BrandController@index'
+        );
+    }
+}
