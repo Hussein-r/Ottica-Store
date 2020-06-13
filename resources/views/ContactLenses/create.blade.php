fc2906f24b7ce01bcd70544e0342b27db6733bc7
haidy <mhaidy718@gmail.com>
2020-06-13 13:04:53 +0200

    a

--- resources/views/ContactLenses/create.blade.php
+++ resources/views/ContactLenses/create.blade.php
@@ -7,14 +7,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" type="text/css" href="/css/nunito-font.css"/>
     <link rel="stylesheet" href="/css/style.css"/>
+    <style>
+    .small-middle-container{
+	margin: auto;
+	width: 40%;
+  }
+</style>
 </head>
     <body class="form-v6">
     <h1 style="text-align: center">Add New Contact Lense</h1>
-        <div class="container" >
+        <div  class="small-middle-container" >
             
             {!! Form::open(['route' => 'lenses.store','files' => 'true', 'method'=>'post']) !!}
             
-                <div class="input-group mb-3">
+                <div class="input-group mb-3 ">
                  
                     {!! Form::text('name',null,['class'=>'form-control','aria-label'=>'name', 'aria-describedby'=>'basic-addon1','placeholder'=>'Contact Name'])  !!}
                 </div>
@@ -33,8 +39,8 @@
                
                <div class="input-group mb-3">
                     
-                    <select class="custom-select" id="inputGroupSelect01"name="color[]" multiple>
-                        
+                    <select class="input-group input-group-sm "  id="inputGroupSelect01"name="color[]" multiple>
+                    <option disabled>Choose Color</option>
                         @if ($colors->count())
                             @foreach($colors as $color)
                                     <option value="{{ $color->id }}">{{ $color->name }}</option> 
@@ -117,27 +123,30 @@
                     {!! Form::text('water_of_content',null,['class'=>'form-control','aria-label'=>'water_of_content', 'aria-describedby'=>'basic-addon1','placeholder'=>'Water Of Content'])  !!}
                 </div>
                 <div><span class="text-white">{{$errors->first('water_of_content')}}</span></div>
+                <div class="input-group mb-3">
                 <select class="custom-select" id="inputGroupSelect01" name="lense_purpose">
                         <option disabled>Choose Lense Purpose</option>
                         <option  value="1">medical</option> 
                         <option  value="2">beauty</option>        
                 </select>
                  <div ><span class="text-white">{{$errors->first('lense_purpose')}}</span></div> 
-                 <div class="input-group mb-3">
+                </div>
+                <div class="input-group mb-3">
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="basic-addon1">Best seller</span>
-                    </div>   
-                    {!! Form::checkbox('best_seller', '0', null, ['class'=>'form-control','Hidden']) !!}
+                    </div> 
                     {!! Form::checkbox('best_seller', '1', null, ['class'=>'form-control']) !!}
-                 </div>
-                <div style=margin:18px;>
+                   
+                </div>
+                <div class="input-group mb-3">
+               
                     <div class="custom-file" >
                         <input type="file" class="custom-file-input" name="images[]" id="inputGroupFile01" multiple>
                         <label class="custom-file-label" for="inputGroupFile01">Choose Image</label>
                     </div>
-              
-                     
-                </div>
+                    </div>
+                   
+                
 
         <div  style="text-align: center "> 
          <button type="submit" class="btn btn-primary center-block btn-lg" >ADD</button>
