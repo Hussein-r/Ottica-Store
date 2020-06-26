7128fb4e1b32258c47839ed4e6e0263c0807b4bf
hussein-r <Hussein_rk94@outlook.com>
2020-06-26 21:36:18 +0200

    middleware

--- app/Http/Controllers/SpecialOffersController.php
+++ app/Http/Controllers/SpecialOffersController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class SpecialOffersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('adminaccess')->except('list');
+    }
     /**
      * Display a listing of the resource.
      *
