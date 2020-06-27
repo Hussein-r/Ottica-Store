edcdd668ddffeff53fd06e7c9c08b3da7325733c
hussein-r <Hussein_rk94@outlook.com>
2020-06-27 16:27:13 +0200

    glasses style

--- resources/views/glass/sunglass.blade.php
+++ resources/views/glass/sunglass.blade.php
@@ -11,80 +11,76 @@
 @section('content')
 <body style="background-color:white;">
   <section class="container">
-    <div class="site-blocks-cover" data-aos="fade">
-      <div class="container">
-        <div class="row">
+    <div>
+      <div class="site-blocks-cover" data-aos="fade">
+        <div class="container">
+          <div class="row">
             <div class="col-md-6 ml-auto order-md-2 align-self-start">
-                <div class="site-block-cover-content">
-                    <h2 class="sub-title">Ottica Store</h2>
-                    <h1>Sun Glasses</h1>
-                    <p><a href="#" class="btn btn-black rounded-0">Shop Now</a></p>
-                </div>
+              <div class="site-block-cover-content">
+                  <h2 class="sub-title">Ottica Store</h2>
+                  <h1>Sun Glasses</h1>
+                  <p><a href="#" class="btn btn-black rounded-0">Shop Now</a></p>
+              </div>
             </div>
             <div class="col-md-6 order-1 align-self-end">
                 <img src="/images/model_3.png" alt="Image" class="img-fluid">
             </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="product-sorting d-flex mt-3" style="float:right;">
-      
-          <select name="select" id="sortByPrice">
-              <option value="" disabled selected>Sorting by Price</option>
-              <option value="low">Price: Low - High</option>
-              <option value="high">Price: High - Low</option>
-          </select>
-          <input type="hidden" id='glassType' value="sun" name="type"/>
-          
+      <div class="product-sorting d-flex mt-3" style="float:right;">
+        <select name="select" id="sortByPrice">
+            <option value="" disabled selected>Sorting by Price</option>
+            <option value="low">Price: Low - High</option>
+            <option value="high">Price: High - Low</option>
+        </select>
+        <input type="hidden" id='glassType' value="sun" name="type"/>
+      </div>
     </div>
     <div class="site-section ">
       <div class="container">
-        <div class="row mb-5">
-          {{-- <div class="col-9"> --}}
-            <div id="filter_data" class="col-9">
-              <div id="glassArea">
+        <div class="row">
+          <div>
+            <div id="filter_data" class="col-9" style="display:inline-block;float:right;">
+              <div id="glassArea" >
                 @foreach ($glasses as $glass)
-                <div class="single-product-wrapper  mt-8 col-4 h-30" style="display:inline-block;">
-                  <div class="product-img" style="border: 1px solid rgb(243, 243, 243)">
-                      <img src="images/{{$glass->images->first()->image}}" alt="">
-                      {{-- <div class="product-badge new-badge">
-                        <span>{{$glass->label}}</span>
-                        </div> --}}
-                      <div class="product-favourite">
-                          <a {{ $glass->favourite->count() ? "style=color:red;" : ''}} id="love"  onclick="return(updateFavorite({{$glass->id}},this))" class="favme fa fa-heart"></a>
-                      </div>
+                <div class="single-product-wrapper" style="display:inline-block;width:260px;height:350px;">
+                  <div class="product-img" style="border: 1px solid rgb(243, 243, 243);height:200px;">
+                    <img src="images/{{$glass->images->first()->image}}" alt="" class="mt-4">
+                    <div class="product-badge new-badge">
+                      <span>{{$glass->label}}</span>
+                    </div>
+                    <div class="product-favourite">
+                      <a {{ $glass->favourite->count() ? "style=color:red;" : ''}} id="love"  onclick="return(updateFavorite({{$glass->id}},this))" class="favme fa fa-heart"></a>
+                    </div>
                   </div>
                   <div class="product-description" style="background-color:rgb(247, 247, 247); padding:10px">
-                      {{-- <span>{{$glass->glass_code}}</span> --}}
-                          <h3 >{{$glass->brand->name}}</h6>
-                      
-                      {{-- <span>Price:  </span>  --}}
-                      <p class="product-price"><span>{{$glass->price_before_discount}} EGP</span> 
-                              {{$glass->price_after_discount}} EGP<span>
+                    <h3>{{$glass->brand->name}}</h6>
+                    <p class="product-price"><span>{{$glass->price_before_discount}} EGP</span> 
+                    {{$glass->price_after_discount}} EGP<span>
                     <h5 class="text-danger" style="text-align: right">{{round((($glass->price_before_discount - $glass->price_after_discount)/$glass->price_before_discount)*100) }} %</h5></span></p>
-                      <!-- Hover Content -->
-                      <div class="hover-content">
-                          <!-- Add to Cart -->
-                          <div class="add-to-cart-btn">
-                              <a href="/glass/{{$glass->id}}" class="btn essence-btn">View Details</a>
-                          </div>
+                    <!-- Hover Content -->
+                    <div class="hover-content">
+                      <div class="add-to-cart-btn">
+                        <a href="/glass/{{$glass->id}}" class="btn essence-btn">View Details</a>
                       </div>
+                    </div>
                   </div>
                 </div>
                 @endforeach  
                 <div style="text-align: center;">
                   {{ $glasses->links() }}
-              </div>
+                </div>
               </div>
             </div>
-            <div class="col-3">
-            {{-- <div class="col-md-4 order-2 mb-5 mb-md-0" style="display:inline-block;float:left;"> --}}
+            <div class="col-3" style="display:inline-block;float:left;">
+            {{-- <div class="col-md-4 order-2 mb-5 mb-md-0" > --}}
               <div class="border p-4 rounded mb-4 filteration">
                 <h3 class="mb-3 h6 text-uppercase text-black d-block">Price</h3>
                 <ul class="list-unstyled mb-0" >
                   <li class="mb-1"> <input type="checkbox"  id="maximum_price" value="1"><span> Less Than 500 </span></li>
-                  <li class="mb-1"> <input type="checkbox"  id="maximum_price"  value="2"><span>500:1000 </span></li>
-                  <li class="mb-1"> <input type="checkbox"  id="maximum_price" value="3"><span>More Than 1000 </span></li>
+                  <li class="mb-1"> <input type="checkbox"  id="maximum_price"  value="2"><span> 500:1000 </span></li>
+                  <li class="mb-1"> <input type="checkbox"  id="maximum_price" value="3"><span> More Than 1000 </span></li>
                 </ul>
               </div>
               <div class="border p-4 rounded mb-4 filteration">
@@ -160,7 +156,7 @@
                 </ul>
               </div>
             </div>
-          {{-- </div> --}}
+          </div>
         </div>
       </div>
     </div>
