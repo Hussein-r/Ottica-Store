0999bec2f108eae2015940496d2139d2dad0fde4
hussein-r <Hussein_rk94@outlook.com>
2020-06-28 03:28:59 +0200

    orderlist

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -81,12 +81,15 @@
                     <div class="form-group">
                         <label for="card-element">Credit Card</label>
                         <div id="card-element">
+
                           <!-- a Stripe Element will be inserted here. -->
-                      </div>
+                        </div>
 
                       <!-- Used to display form errors -->
-                      <div id="card-errors" role="alert"></div>
+                        <div id="card-errors" role="alert"></div>
+                        
                     </div>
+                    
 
                     <div class="spacer"></div>
 
